f8a98dec0d4cbbf6dd4a178aacb6b3096f50ef48
Anton Zakharov <aznatkohnarov@gmail.com>
2024-07-18 22:19:50 +0300

    Это наш первый коммит в проекте

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+from handlers import other_handlers, user_handlers, labs_handlers
+from aiogram.client.default import DefaultBotProperties
+
+async def main():
+    config: Config = load_config()
+    bot = Bot(token=config.tg_bot.token, default=DefaultBotProperties(parse_mode='HTML'))
+    dp = Dispatcher()
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
