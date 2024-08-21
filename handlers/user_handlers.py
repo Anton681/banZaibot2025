39f11308fc2fcffbaafe351992d03b44189658bd
Anton681 <aznatkohnarov@gmail.com>
2024-08-21 13:07:14 +0300

    ffs

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -164,7 +164,8 @@ async def process_dog_answer(message: Message):
 
 @router.message(F.text == 'Аптеки💊')
 async def process_dog_answer(message: Message):
-    await message.answer_photo(photo=photo_ids['apt'], caption=okrest_txt['apt'], reply_markup=keyboard251, link_preview_options=options_1)
+    await message.answer_photo(photo=photo_ids['apt'])
+    await message.answer(caption=okrest_txt['apt'], reply_markup=keyboard251, link_preview_options=options_1)
 
 @router.message(F.text == 'Развлечения и отдых🎡')
 async def process_dog_answer(message: Message):
